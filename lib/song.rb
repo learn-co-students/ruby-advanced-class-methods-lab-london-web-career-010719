9a98e6c6ec040b58bdebbdf1cccfd1d00617f1ac
Flatiron <flatiron@flatirons-air-2.home>
2019-01-13 22:59:21 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,10 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize
+    @@all << self
+  end
+
   def self.all
     @@all
   end
@@ -10,4 +14,58 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name (name)
+    song = Song.new
+    song.name= name
+    #@@all << song
+    song
+  end
+
+  def self.create_by_name (name)
+    song = Song.new
+    song.name= name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name (name)
+    @@all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name (name)
+    self.create_by_name(name)
+    self.find_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by! {|song| song.name}.uniq
+  end
+
+  def self.new_from_filename (filename)
+    artist_name = filename[0..filename.index("-")-2]
+    song_name = filename[filename.index("-")+2..filename.index(".")-1]
+    song = Song.new
+    song.name= song_name
+    song.artist_name= artist_name
+    song
+  end
+
+  def self.create_from_filename (filename)
+    artist_name = filename[0..filename.index("-")-2]
+    song_name = filename[filename.index("-")+2..filename.index(".")-1]
+    song = Song.new
+    song.name= song_name
+    song.artist_name= artist_name
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
